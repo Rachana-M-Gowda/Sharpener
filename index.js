acbdf4955cac7be3d3f6e60a519990a5587dacbd
Rachana-M-Gowda <gowdarachanam@gmail.com>
2025-07-11 14:48:16 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,35 +1,21 @@
 const express = require('express');
 const app = express();
 
-app.use(express.json()); // For parsing JSON if needed
-
 const PORT = 4000;
 
-// Products Routes
-app.get('/products', (req, res) => {
-  res.send('Here is the list of all products.');
+// Dynamic Route: /welcome/:username
+app.get('/welcome/:username', (req, res) => {
+  const username = req.params.username; // route parameter
+  const role = req.query.role; // query parameter
+
+  if (role) {
+    res.send(`Welcome ${username}, your role is ${role}`);
+  } else {
+    res.send(`Welcome ${username}`);
+  }
 });
 
-app.post('/products', (req, res) => {
-  res.send('A new product has been added.');
-});
-
-// Categories Routes
-app.get('/categories', (req, res) => {
-  res.send('Here is the list of all categories.');
-});
-
-app.post('/categories', (req, res) => {
-  res.send('A new category has been created.');
-});
-
-// Wildcard Route for Undefined Paths
-app.use((req, res) => {
-  res.status(404).send('<h1>404 - Page Not Found</h1>');
-});
-
-
-// Start Server
+// Start server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
