878fdf0f2ebafd88a0447c488e2ac98f0256ce78
Rachana-M-Gowda <gowdarachanam@gmail.com>
2025-07-11 13:04:12 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 const express = require('express');
 const app = express();
 
-// Middleware to parse JSON (in case we use JSON in the future)
-app.use(express.json());
+app.use(express.json()); // For parsing JSON if needed
 
-// GET /orders
-app.get('/orders', (req, res) => {
-  res.send("Here is the list of all orders.");
+const PORT = 4000;
+
+// Products Routes
+app.get('/products', (req, res) => {
+  res.send('Here is the list of all products.');
 });
 
-// POST /orders
-app.post('/orders', (req, res) => {
-  res.send("A new order has been created.");
+app.post('/products', (req, res) => {
+  res.send('A new product has been added.');
 });
 
-// GET /users
-app.get('/users', (req, res) => {
-  res.send("Here is the list of all users.");
+// Categories Routes
+app.get('/categories', (req, res) => {
+  res.send('Here is the list of all categories.');
 });
 
-// POST /users
-app.post('/users', (req, res) => {
-  res.send("A new user has been added.");
+app.post('/categories', (req, res) => {
+  res.send('A new category has been created.');
 });
 
-// Start the server
-const PORT = 3000;
+// Wildcard Route for Undefined Paths
+app.use((req, res) => {
+  res.status(404).send('<h1>404 - Page Not Found</h1>');
+});
+
+
+// Start Server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
