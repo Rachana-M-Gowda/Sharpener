ef2a3604caff473fdf34dd3a39a7d9e1c0aaffd0
Rachana-M-Gowda <gowdarachanam@gmail.com>
2025-07-11 12:51:41 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+// Middleware to parse JSON (in case we use JSON in the future)
+app.use(express.json());
+
+// GET /orders
+app.get('/orders', (req, res) => {
+  res.send("Here is the list of all orders.");
 });
 
-const PORT = 3000;
+// POST /orders
+app.post('/orders', (req, res) => {
+  res.send("A new order has been created.");
+});
 
+// GET /users
+app.get('/users', (req, res) => {
+  res.send("Here is the list of all users.");
+});
+
+// POST /users
+app.post('/users', (req, res) => {
+  res.send("A new user has been added.");
+});
+
+// Start the server
+const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server is up and running on port ${PORT}! Ready to handle requests.`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
