b9a8543f5bd99de21c37cfec47b4a6940b61e776
Rachana-M-Gowda <gowdarachanam@gmail.com>
2025-07-12 17:44:31 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
 // server.js
+
 const express = require('express');
 const app = express();
 const port = 3000;
 
 // Import route files
-const userRoutes = require('./routes/userRoutes');
-const productRoutes = require('./routes/productRoutes');
-const cartRoutes = require('./routes/cartRoutes');
+const userRoutes = require('./routes/userRoute');
+const productRoutes = require('./routes/productRoute');
+const cartRoutes = require('./routes/cartRoute');
 
-// Middleware
+// Middleware to parse JSON request bodies
 app.use(express.json());
 
-// Use routes
-app.use('/users', userRoutes);
-app.use('/products', productRoutes);
-app.use('/cart', cartRoutes);
+// Use route files with /api prefix
+app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/cart', cartRoutes);
 
-// Start server
+// Default route
+app.get('/', (req, res) => {
+  res.send('Welcome to the E-Commerce API');
+});
+
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
