fb1bcfbf88e022b58e47880378cb81b7faa26572
Rachana-M-Gowda <gowdarachanam@gmail.com>
2025-07-12 19:28:32 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
+// server.js
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-const productRoutes = require('./routes/productRoutes');
-
-app.use(express.json());
-app.use('/api/products', productRoutes);
-
-app.get('/', (req, res) => {
-  res.send('Welcome to the E-Commerce API!');
+// GET endpoint to serve HTML file
+app.get('/api/products', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'products.html'));
 });
 
+// Default route
+app.get('/', (req, res) => {
+  res.send('Welcome to the HTML API server!');
+});
+
+// Start server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
