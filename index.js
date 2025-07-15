9dbb0765518d4e341b10c0f929ce04c674d21537
Rachana-M-Gowda <gowdarachanam@gmail.com>
2025-07-15 10:39:39 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
-const express = require('express');
-const app = express();
-const PORT = 3000;
+const express=require('express')
+const mysql=require('mysql2')
+const app=express();
 
-// Routers
-const studentRoutes = require('./routes/studentRoutes');
-const courseRoutes = require('./routes/courseRoutes');
+const connection=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'mysql@2025',
+    database:'testdb'
+})
 
-// Home route
-app.get('/', (req, res) => {
-  res.send("Welcome to the Student & Course Portal API!");
-});
+connection.connect((err)=>{
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log('connection has been created');
 
-// Mount routers
-app.use('/students', studentRoutes);
-app.use('/courses', courseRoutes);
+    const creationQuery=`create table Student(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(20),
+    email VARCHAR(20)
+)`
+    connection.execute(creationQuery,(err)=>{
+        if(err){
+            console.log(err);
+            connection.end();
+            return;
+        }
+        console.log('Table is created');
+    })
 
-// 404 Not Found Handler
-app.use((req, res) => {
-  res.status(404).send("Page not found");
-});
+})
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+app.get('/',(req,res)=>{
+    res.send('Hello world');
+})
+
+app.listen(3000,(err)=>{
+    console.log("Server is running");
+})
