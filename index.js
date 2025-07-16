0de3527e9e0944fc259b9f92291322f3df5e7e0a
Rachana-M-Gowda <gowdarachanam@gmail.com>
2025-07-16 10:08:47 +0530

    Add files via upload
    sequelize

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-const express = require('express');
-const app = express();
-const studentRoutes = require('./routes/studentRoutes'); // Correct path
+const express=require('express')
+const db=require('./utils/db-connection');
+const studentRoutes=require('./routes/studentsRoutes');
+const app=express();
+
+
+//models
+const studentModel=require('./models/students')
 
 app.use(express.json());
 
 app.get('/',(req,res)=>{
-  res.send('Hello world');
+    res.send('Hello world');
 })
-app.use('/students', studentRoutes); // Route setup
 
-app.listen(3000, (err) => {
-  console.log('Server started at http://localhost:3000');
+app.use("/students",studentRoutes);
+
+db.sync({force:true}).then((err)=>{
+    app.listen(3000,(err)=>{
+        console.log("Server is running");
+    })
+}).catch((err)=>{
+    console.log(err);
+})
+
+app.listen(3000,(err)=>{
+    console.log("Server is running");
 })
--- utils/db-connection.js
+++ utils/db-connection.js
@@ -1,37 +1,62 @@
+const {Sequelize}=require('sequelize');
 
-const mysql = require('mysql2')
+const sequelize=new Sequelize('testdb','root','mysql@2025',{
+    host:'localhost',
+    dialect:'mysql'
+}) ;
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'mysql@2025',
-  database: 'studentdb'
-})
+(async ()=>{try {
+    await sequelize.authenticate();
+    console.log("connection to the database has been created");
+} catch (error) {
+    console.log(error);
+}})();
 
-connection.connect((err)=>{
-    if(err){
-        console.log(err);
-        return;
-    }
-    console.log('connection has been created');
-
-    const creationQuery=`CREATE TABLE IF NOT EXISTS STUDENTS (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    email VARCHAR(255) UNIQUE,
-    age INT
-)`
-    connection.execute(creationQuery,(err)=>{
-        if(err){
-            console.log(err);
-            connection.end();
-            return;
-        }
-        console.log('Table is created');
-    })
-
-})
-module.exports=connection;
+module.exports=sequelize;
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+// const mysql=require('mysql2')
+
+
+// const connection=mysql.createConnection({
+//     host:'localhost',
+//     user:'root',
+//     password:'mysql@2025',
+//     database:'testdb'
+// })
+
+// connection.connect((err)=>{
+//     if(err){
+//         console.log(err);
+//         return;
+//     }
+//     console.log('connection has been created');
+
+//     const creationQuery=`create table IF NOT EXISTS Student(
+//     id INT AUTO_INCREMENT PRIMARY KEY,
+//     name VARCHAR(20),
+//     email VARCHAR(20)
+// )`
+//     connection.execute(creationQuery,(err)=>{
+//         if(err){
+//             console.log(err);
+//             connection.end();
+//             return;
+//         }
+//         console.log('Table is created');
+//     })
+
+// })
+// module.exports=connection;
