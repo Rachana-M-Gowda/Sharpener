d35b8e917683ce7049ff5802e6e209d33a2f3ea0
Rachana-M-Gowda <gowdarachanam@gmail.com>
2025-07-16 15:16:50 +0530

    Add files via upload
    Full stack Booking Appointment App

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require('express');
+const cors = require('cors');
+const path = require('path');
 const { sequelize } = require('./models');
 const userRoutes = require('./routes/userRoutes');
-const busRoutes = require('./routes/busRoutes');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/users', userRoutes);
-app.use('/buses', busRoutes);
 
-sequelize.sync().then(() => {
-  app.listen(3000, () => {
-    console.log('✅ Server is running at http://localhost:3000');
-  });
-});
+// Sync DB and Start Server
+
+sequelize.sync({ force: true })  // DANGER: This drops and recreates all tables
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running at http://localhost:3000");
+    });
+  })
+  .catch(err => console.log(err));
