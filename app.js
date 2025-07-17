ebe4848a593e4b78f54f34ba0d09dd5f53dce9c2
Rachana-M-Gowda <gowdarachanam@gmail.com>
2025-07-17 10:10:43 +0530

    Add files via upload
    1:M association

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-const http = require('http');
-const routes=require('./routes')
+const express=require('express')
+const db=require('./utils/db-connection');
+const studentRoutes=require('./routes/studentsRoutes');
+const app=express();
 
-routes.testFunction();
-const server = http.createServer(routes.handler);
 
-server.listen(3001, () => {
-  console.log("Server is running on http://localhost:3000");
-});
+//models
+require('./models')
+
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send('Hello world');
+})
+
+app.use("/students",studentRoutes);
+
+db.sync({force:true}).then((err)=>{
+    app.listen(3000,(err)=>{
+        console.log("Server is running");
+    })
+}).catch((err)=>{
+    console.log(err);
+})
+
+app.listen(3000,(err)=>{
+    console.log("Server is running");
+})
