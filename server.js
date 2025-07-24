89fe8276c8109b5aba42dfa9604b326b816f355c
Rachana-M-Gowda <gowdarachanam@gmail.com>
2025-07-24 22:10:29 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,22 +1,17 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
-const sequelize = require('./config/db');
-const authRoutes = require('./routes/authRoutes');
-const path = require('path');
-
 const app = express();
-const PORT = 3000;
+const path = require('path');
+const { sequelize } = require('./models');
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
-app.use('/api', authRoutes);
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/expenses', require('./routes/expenseRoutes'));
 
-// Start
-sequelize.sync().then(() => {
-  console.log('DB synced');
-  app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+sequelize.authenticate().then(() => {
+  console.log('DB connected');
+  app.listen(3000, () => console.log('Server running on 3000'));
 });
