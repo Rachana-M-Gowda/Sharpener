e7b9a1b568731bf4c38d9f99dfedbe4cd685c56b
Rachana-M-Gowda <gowdarachanam@gmail.com>
2025-08-05 11:28:16 +0530

    Add files via upload
    Expense Tracker

--- server.js
+++ server.js
@@ -1,17 +1,48 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+require("dotenv").config();
+const cors = require("cors");
+const morgan = require("morgan");
+
 const app = express();
-const path = require('path');
-const { sequelize } = require('./models');
+const PORT = process.env.PORT || 3000;
 
+//  Import route files
+const authRoutes = require("./server/routes/auth");
+const paymentRoutes = require("./server/routes/payment");
+const leaderboardRoutes = require("./server/routes/leaderboard");
+const expenseRoutes = require("./server/routes/expenses");
+const passwordRoutes = require("./server/routes/password");
+
+//  Middleware
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan("dev"));
 
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/expenses', require('./routes/expenseRoutes'));
+//  Serve signup page at root
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "signup-page.html"));
+});
+app.get("/logout", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "logout.html"));
+});
 
-sequelize.authenticate().then(() => {
-  console.log('DB connected');
-  app.listen(3000, () => console.log('Server running on 3000'));
+//  Mount all routes
+app.use(authRoutes);
+app.use(paymentRoutes);
+app.use("/leaderboard", leaderboardRoutes);
+app.use(expenseRoutes);
+app.use("/password", passwordRoutes);
+
+//  Error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Something went wrong!" });
+});
+
+//  Start server
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(` Server running at http://localhost:${PORT}/`);
 });
